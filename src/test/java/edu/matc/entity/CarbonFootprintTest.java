d3a9d4a73a805fa876112a18cbf264ebf3e5392a
Emily Gimm <egimm@madisoncollege.edu>
2025-02-19 21:22:24 -0600

    adding CarbonFootprint Test

---
+++ src/test/java/edu/matc/entity/CarbonFootprintTest.java
@@ -0,0 +1,60 @@
+package edu.matc.entity;
+
+import edu.matc.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarbonFootprintTest {
+
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("cleanDB.sql");
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void setUser() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void setAmount() {
+    }
+
+    @Test
+    void getEntryDate() {
+    }
+
+    @Test
+    void setEntryDate() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
