54db2e7daefc28e76f0339f044d0a6a3d2b637e0
Emily Gimm <egimm@madisoncollege.edu>
2025-02-24 10:14:19 -0600

    adding calculate emissions

--- src/main/java/edu/matc/controller/CalculateEmissions.java
+++ src/main/java/edu/matc/controller/CalculateEmissions.java
@@ -1,7 +1,9 @@
+
+
 package edu.matc.controller;
 
 /**
- * The type Transportation.
+ * The CalculateEmissions class calculates emissions for transportation, home energy, and waste.
  *
  * @author egimm
  */
@@ -11,19 +13,47 @@ public class CalculateEmissions {
     private static final double CO2_EMISSIONS_PER_GALLON = 18.73;
     private double averageMonthlyElectricBill;
     private double pricePerKWh;
-
+    private static final double ELECTRICITY_EMISSION_FACTOR = 0.92; // check for exact emission factor
+    private static final int MONTHS_IN_A_YEAR = 12;
 
 
     /**
-     * Instantiates a new Transportation.
+     * Instantiates a new constructor for Transportation.
      *
      * @param milesPerGallon    the miles per gallon
      * @param milesDrivenWeekly the miles driven weekly
      */
     public CalculateEmissions(double milesPerGallon, double milesDrivenWeekly) {
-    this.milesDrivenWeekly = milesDrivenWeekly;
-    this.milesPerGallon = milesPerGallon;
-}
+        this.milesDrivenWeekly = milesDrivenWeekly;
+        this.milesPerGallon = milesPerGallon;
+    }
+
+
+    /**
+     * Constructor for home energy emissions
+     *
+     * @param averageMonthlyElectricBill the average monthly electric bill
+     */
+    public CalculateEmissions(double averageMonthlyElectricBill, double pricePerKWh) {
+        this.averageMonthlyElectricBill = averageMonthlyElectricBill;
+        this.pricePerKWh = pricePerKWh;
+    }
+
+
+    /**
+     * Constructor for both Transportation and Home Energy emissions.
+     *
+     * @param milesPerGallon    the miles per gallon
+     * @param milesDrivenWeekly the miles driven weekly
+     * @param averageMonthlyElectricBill the average monthly electric bill
+     * @param pricePerKWh       the price per kWh
+     */
+    public CalculateEmissions(double milesPerGallon, double milesDrivenWeekly, double averageMonthlyElectricBill, double pricePerKWh) {
+        this(milesPerGallon, milesDrivenWeekly);
+        this.averageMonthlyElectricBill = averageMonthlyElectricBill;
+        this.pricePerKWh = pricePerKWh;
+    }
+
 
     /**
      * Calculate fuel consumption double.
@@ -32,27 +62,34 @@ public class CalculateEmissions {
      */
     public double calculateFuelConsumption() {
 
-    return milesDrivenWeekly / milesPerGallon;
+        return milesDrivenWeekly / milesPerGallon;
 
-}
+    }
 
     /**
-     * Calculate emissions of gas car double.
+     * Calculate emissions of gas car.
      *
-     * @return the double
+     * @return the CO2 emissions in lbs
      */
     public double calculateEmissionsOfGasCar() {
 
-    return calculateFuelConsumption() * CO2_EMISSIONS_PER_GALLON;
+        return calculateFuelConsumption() * CO2_EMISSIONS_PER_GALLON;
 
-}
+    }
 
     /* Add calculation of emissions of greenhouse gasses other than CO2 */
 
 
+    /**
+     * Calculate home energy consumption emissions.
+     *
+     * @return the CO2 emissions in lbs
+     */
     public double calculateHomeEnergyConsumption() {
-
-        return (averageMonthlyElectricBill / pricePerKWh) * electricityEmissionFactor * monthsInAYear;
+        if (pricePerKWh == 0) {
+            throw new IllegalArgumentException("Price per kWh cannot be zero.");
+        }
+        return (averageMonthlyElectricBill / pricePerKWh) * ELECTRICITY_EMISSION_FACTOR * MONTHS_IN_A_YEAR;
 
 
         // If user uses green power, multiply by percentage of green power used
@@ -60,17 +97,27 @@ public class CalculateEmissions {
         // does your house use green power? what portion of your households total purchased electricity uses green power
     }
 
+// 822 lbs is the average for 1 person
 
+    /**
+     * Calculate total average waste emissions.
+     *
+     * @return the CO2 emissions from waste in lbs.
+     */
     public double calculateTotalAverageWasteEmissions() {
-        const int TOTAL_WASTE_EMISSIONS_BEFORE_RECYCLING = 822;
-        const int EMISSIONS_FROM_NEWSPAPER = ;
-        const int EMISSIONS_FROM_ALUM = ;
-        const int EMISSIONS_FROM_PLASTIC = ;
-        const int EMISSIONS_FROM_GLASS = ;
-        const int EMISSIONS_FROM_MAGAZINES = ;
+        final int TOTAL_WASTE_EMISSIONS_BEFORE_RECYCLING = 822;
+        final int EMISSIONS_FROM_NEWSPAPER = 119;
+        final int EMISSIONS_FROM_ALUM = 121;
+        final int EMISSIONS_FROM_PLASTIC = 73;
+        final int EMISSIONS_FROM_GLASS = 19;
+        final int EMISSIONS_FROM_MAGAZINES = 85;
 
-        TOTAL_WASTE_EMISSIONS_BEFORE_RECYCLING + EMISSIONS_FROM_NEWSPAPER + EMISSIONS_FROM_ALUM
-                    + EMISSIONS_FROM_PLASTIC + EMISSIONS_FROM_GLASS + EMISSIONS_FROM_MAGAZINES;
+        return TOTAL_WASTE_EMISSIONS_BEFORE_RECYCLING
+                + EMISSIONS_FROM_NEWSPAPER
+                + EMISSIONS_FROM_ALUM
+                + EMISSIONS_FROM_PLASTIC
+                + EMISSIONS_FROM_GLASS
+                + EMISSIONS_FROM_MAGAZINES;
     }
 
 }
